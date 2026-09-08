2602e44a28709c86cda26d61767d870ed6a67e3c
Josh Bello <Jb230913@live.com>
2026-09-08 19:47:14 +0100

    update - added html boilerplate and script to layout

--- resources/views/layouts/back-end.blade.php
+++ resources/views/layouts/back-end.blade.php
@@ -1,3 +1,48 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Contact - @yield('title')</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    <link rel="stylesheet" href=" {{ URL::asset('css/reset.css') }}">
+    <link rel="stylesheet" href=" {{ URL::asset('css/layout.css') }}">
+    <link rel="stylesheet" href=" {{ URL::asset('css/mobile_nav.css') }}">
+    <link rel="stylesheet" href=" {{ URL::asset('css/mobile_nav_bootstrap_custom.css') }}">
+    <link rel="stylesheet" href=" {{ URL::asset('css/sidebar.css') }}">
+    @stack('styles')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href=" {{ URL::asset('css/admin_layout.css') }}">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
+    @else
+
+    @endif
+</head>
+
+<body class="d-flex flex-column w-100 vh-100 bg-body-tertiary">
+    @include('include.admin_panel_mobile')
+
+    <main class="container-fluid ">
+        <div class="row">
+            @include('include.admin_panel')
+
+            <main class="admin-main col-md-10">
+                @yield('content')
+            </main>
+        </div>
+
+
+    </main>
+    @include('include.footer')
+    @stack('scripts')
+    <script src="{{ URL::asset('js/mobile_nav.js') }}"></script>
+    <script src="{{ URL::asset('js/admin_nav.js') }}"></script>
+</body>
+</html>
