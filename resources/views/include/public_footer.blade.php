065c5f854bdde5fcbecba5de4bdc7331ae526053
Josh Bello <Jb230913@live.com>
2026-09-08 13:44:33 +0100

    add - new files for components and page and partials

---
+++ resources/views/include/public_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4  | position-absolute w-100 bottom-0">
+    <p class="col-md-4 mb-0 text-body-secondary">© 2026 MyContact</p> <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" aria-label="Bootstrap"> <svg class="bi me-2" width="40" height="32" aria-hidden="true">
+            <use xlink:href="#bootstrap"></use>
+        </svg> </a>
+
+</footer>
