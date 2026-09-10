52eb130baf0eb2d8552d14f3a7cf74c019af4ca8
Josh Bello <Jb230913@live.com>
2026-09-10 22:22:37 +0100

    update - added dummy testing code to simulate data of the page

--- resources/views/backend/passwordCategory/index.blade.php
+++ resources/views/backend/passwordCategory/index.blade.php
@@ -5,31 +5,45 @@
 @section('title','Dashboard')
 @section('content')
 <div class="container-fluid p-4">
-            <div class="row">
+    <div class="row">
         <h1 class="text-center">Categories</h1>
     </div>
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Title</th>
-      <th scope="col">No of Tagged Password</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>
-        <a type="button" class="btn" href="">Edit</a>
-        <a type="button" class="btn" href="">Delete</a>
-      </td>
-    </tr>
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">No of Tagged Password</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+            $categories = [
+            ["title"=> "Algebra",
+            "count" => 8],
+            ["title"=> "Algebra",
+            "count" => 5],
+            ["title"=> "Algebra",
+            "count" => 4],
+            ["title"=> "Algebra",
+            "count" => 9],
+            ]
+            @endphp
+            <tr>
+                @foreach($categories as $key => $value)
 
-  </tbody>
-</table>
+                <th scope="row">1</th>
+                <td>{{ $value['title'] }}</td>
+                <td>{{ $value['count'] }}</td>
+                <td>
+                    <a type="button" class="btn" href="">Edit</a>
+                    <a type="button" class="btn" href="">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
 
