b8ea3cbdd4abe190d457851c56e6931a2bd947cc
Josh Bello <Jb230913@live.com>
2026-09-11 20:19:44 +0100

    update - added logic for displaying data and buttons

--- resources/views/components/list.blade.php
+++ resources/views/components/list.blade.php
@@ -1,8 +1,21 @@
   @props(['categories'])
-      <ul {{ $attributes->merge(['class' =>'']) }}>
-        @foreach($categories as $key => $value)
-              <li class="list-group-item">{{ $value }}</li>
-        @endforeach
-         
+  <ul {{ $attributes->merge(['class' =>'']) }}>
+      @foreach($categories as $key => $value)
+      <li class="list-group-item d-flex justify-content-between">
 
-       </ul>
+          {{ $value->website }}
+          <div>
+              <a type="button" class="btn" href={{ route('backend.password.edit',$value->id) }}>Edit</a>
+              <form action="{{ route('backend.password.delete',$value->id) }}" method="POST" style="display:inline;">
+                  @csrf
+                  @method('DELETE')
+                  {{-- <a type="button" class="btn" href={{ route('backend.password.delete',$value->id) }}></a>Delete</a> --}}
+                 <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this Contact?')">Delete</button>
+              </form>
+          </div>
+
+      </li>
+      @endforeach
+
+
+  </ul>
