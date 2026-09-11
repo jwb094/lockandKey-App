4902f422b767461f1f5f3ec4a74f5670f75d9e8e
Josh Bello <Jb230913@live.com>
2026-09-11 20:22:26 +0100

    add  - new migrations for password table

---
+++ database/migrations/2026_09_11_181338_change_category_id_constrasints_passwords_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('passwords', function (Blueprint $table) {
+            //
+            $table->foreignId('category_id')
+                ->nullable()
+                ->constrained('categories')
+                ->nullOnDelete()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
