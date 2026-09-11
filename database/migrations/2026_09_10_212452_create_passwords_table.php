257b20bc0c780a012c8e41a6342ab559412c3aed
Josh Bello <Jb230913@live.com>
2026-09-11 11:15:29 +0100

    update - modify logic for foriegnKey

--- database/migrations/2026_09_10_212452_create_passwords_table.php
+++ database/migrations/2026_09_10_212452_create_passwords_table.php
@@ -18,10 +18,9 @@ return new class extends Migration
             $table->string('password');
             $table->string('password_hash');
             $table->longText('notes');
-            $table->foreign('category_id')
-                ->references('id')
-                ->on('categories')
-            ->onDelete('cascade');
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->cascadeOnDelete();
             $table->timestamps();
         });
     }
