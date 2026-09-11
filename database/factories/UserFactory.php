e0b966772014bea73f75bc95ace536e5de4b5d4f
Josh Bello <Jb230913@live.com>
2026-09-11 12:04:45 +0100

    update - change structure of record for User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => fake()->password(8, 20),
+            'password_hash' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
