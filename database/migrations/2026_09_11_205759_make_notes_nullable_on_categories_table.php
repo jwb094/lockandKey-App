0b51bec5605c2186a1784085b2296c0cd3818c77
Josh Bello <Jb230913@live.com>
2026-09-11 21:59:34 +0100

    new - migration to focus on notes column in category table

--- database/migrations/2026_09_11_205759_make_notes_nullable_on_categories_table.php
+++ database/migrations/2026_09_11_205759_make_notes_nullable_on_categories_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-               $table->dropColumn('name');
+        Schema::table('categories', function (Blueprint $table) {
+            $table->text('notes')->nullable()->change();
         });
     }
 
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::table('categories', function (Blueprint $table) {
+            $table->text('notes')->nullable(false)->change();
         });
     }
 };
