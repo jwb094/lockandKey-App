8e3529727612bfca3bd780c40a78bb9830c2582a
Josh Bello <Jb230913@live.com>
2026-09-11 13:39:20 +0100

    add - new Service Class for User

---
+++ app/Services/UserService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+
+
+    public function store(array $newUserData): User
+    {
+        $newUserData['password_hash'] = Hash::make($newUserData['password']);
+        $user = User::create($newUserData);
+
+        return $user;
+    }
+
+
+    public function userAuth(array $loginDetails): bool
+    {
+        return Auth::attempt([
+            'email' => $loginDetails['email'],
+            'password' => $loginDetails['password']
+        ]);
+    }
+}
