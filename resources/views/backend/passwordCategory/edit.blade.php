315af3eb86c59bc2342fc4c9012ed871133590b3
Josh Bello <Jb230913@live.com>
2026-09-11 21:47:09 +0100

    update - add route() for action and button component

--- resources/views/backend/passwordCategory/edit.blade.php
+++ resources/views/backend/passwordCategory/edit.blade.php
@@ -13,17 +13,18 @@
     <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="col-md-12">
 
-         <form action="" method="POST">
+         <form action={{ route('backend.category.update',$category) }} method="POST">
                 @csrf
                 @method('put')
                 <div class="mb-3">
                     <x-form-label for="title" class="form-label">Title</x-form-label>
-                    <x-form-input type="text" class="form-control" id="title" name="title" placeholder=""></x-form-input>
+                    <x-form-input type="text" class="form-control" id="title" name="title" placeholder=""  value="{{ old('title',$category->title) }}"></x-form-input>
                 </div>
                 <div class="mb-3">
                     <x-form-label for="notes" class="form-label">Notes</x-form-label>
-                    <x-form-textarea class="form-control" id="notes" name="notes" rows="3"></x-form-textarea>
+                    <x-form-textarea class="form-control" id="notes" name="notes" rows="3">{{ old('title',$category->title) }}</x-form-textarea>
                 </div>
+           <x-form-button class="btn btn-primary w-100 py-2 mt-2" type="submit"> Update</x-form-button>
             </form>
         </div>
     </div>
