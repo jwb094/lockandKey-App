2df04e6b16993473d672e1c10c724a24e91c220a
Josh Bello <Jb230913@live.com>
2026-09-12 10:13:32 +0100

    update - added getAuthPasswordName() and switch attribute for cast()

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,13 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password_hash' => 'hashed',
         ];
+    }
+
+
+    public function getAuthPasswordName(): string
+    {
+        return 'password_hash';
     }
 }
